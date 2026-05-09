b075c3dbdeb28f8b9632a47dd3db1b6daa18f2d2
yadaw-hash <tesfaytewelde2015@gmail.com>
2026-05-09 20:45:15 +0300

    setup vercel deployment

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://mern-auth-app-frontend.vercel.app',
+  'https://mern-auth-service.vercel.app',
 ];
 
 // middlewares
