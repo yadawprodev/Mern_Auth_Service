2a6c220bcc997391d3114e7a2f5884a0437d1df9
yadaw-hash <tesfaytewelde2015@gmail.com>
2026-05-11 18:45:09 +0300

    added changes to the profile dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
@@ -7,9 +7,28 @@ import useAuthStore from '../store/authStore';
 import api from '../lib/axios';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   const { isLoggedIn, setIsLoggedIn, userData, setUserData } = useAuthStore();
 
   const navigate = useNavigate();
+
+  const menuRef = useRef();
+
+  useEffect(() => {
+    // close menu when clicked outside
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   // Function to send verification OTP
   const sendVerificationOtp = async (e) => {
@@ -52,6 +71,49 @@ const Navbar = () => {
     }
   };
 
+  // return (
+  //   <div className='w-full flex justify-between items-center p-4 sm:p-6 absolute top-0 sm:px-24'>
+  //     {/* Logo */}
+  //     <a
+  //       href='/'
+  //       className='flex items-center gap-2 text-lg font-bold text-gray-800'
+  //     >
+  //       <img src={assets.logo} alt='Logo' className='h-10 w-auto sm:h-12' />
+  //       <span>Auth</span>
+  //     </a>
+  //     {userData && userData.username ? (
+  //       <div className='flex w-8 h-8 justify-center items-center rounded-full bg-black text-white relative group cursor-pointer'>
+  //         {userData.username[0].toUpperCase()}
+  //         <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
+  //           <ul className='list-none bg-gray-100 shadow-lg m-0 p-2 text-sm'>
+  //             {!userData.isAccountVerified && (
+  //               <li
+  //                 className='py-1 px-2 hover:bg-gray-200 cursor-pointer'
+  //                 onClick={sendVerificationOtp}
+  //               >
+  //                 Verify Email
+  //               </li>
+  //             )}
+  //             <li
+  //               className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'
+  //               onClick={handleLogout}
+  //             >
+  //               Logout
+  //             </li>
+  //           </ul>
+  //         </div>
+  //       </div>
+  //     ) : (
+  //       <button
+  //         onClick={() => navigate('/login')}
+  //         className='bg-gray-800 hover:bg-gray-500 text-white px-6 py-1.5 rounded-full font-semibold transition-all hover:shadow-md cursor-pointer'
+  //       >
+  //         Login
+  //       </button>
+  //     )}
+  //   </div>
+  // );
+
   return (
     <div className='w-full flex justify-between items-center p-4 sm:p-6 absolute top-0 sm:px-24'>
       {/* Logo */}
@@ -62,27 +124,39 @@ const Navbar = () => {
         <img src={assets.logo} alt='Logo' className='h-10 w-auto sm:h-12' />
         <span>Auth</span>
       </a>
+
       {userData && userData.username ? (
-        <div className='flex w-8 h-8 justify-center items-center rounded-full bg-black text-white relative group cursor-pointer'>
-          {userData.username[0].toUpperCase()}
-          <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
-            <ul className='list-none bg-gray-100 shadow-lg m-0 p-2 text-sm'>
-              {!userData.isAccountVerified && (
-                <li
-                  className='py-1 px-2 hover:bg-gray-200 cursor-pointer'
-                  onClick={sendVerificationOtp}
-                >
-                  Verify Email
-                </li>
-              )}
-              <li
-                className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'
-                onClick={handleLogout}
-              >
-                Logout
-              </li>
-            </ul>
+        <div className='relative' ref={menuRef}>
+          {/* Avatar */}
+          <div
+            onClick={() => setShowMenu((prev) => !prev)}
+            className='flex w-8 h-8 justify-center items-center rounded-full bg-black text-white cursor-pointer'
+          >
+            {userData.username[0].toUpperCase()}
           </div>
+
+          {/* Dropdown */}
+          {showMenu && (
+            <div className='absolute top-10 right-0 z-10 text-black rounded'>
+              <ul className='list-none bg-gray-100 shadow-lg m-0 p-2 text-sm rounded-md min-w-35'>
+                {!userData.isAccountVerified && (
+                  <li
+                    className='py-1 px-2 hover:bg-gray-200 cursor-pointer rounded'
+                    onClick={sendVerificationOtp}
+                  >
+                    Verify Email
+                  </li>
+                )}
+
+                <li
+                  className='py-1 px-2 hover:bg-gray-200 cursor-pointer rounded'
+                  onClick={handleLogout}
+                >
+                  Logout
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       ) : (
         <button
